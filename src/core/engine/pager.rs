4a5c07974ca80285ad37d03f8ddc2d4411306514
c0utin <rafaelcouto111@gmail.com>
2025-11-27 15:55:35 -0300

    aaaaaaaaa

--- src/core/engine/pager.rs
+++ src/core/engine/pager.rs
@@ -1,15 +1,19 @@
+use glommio::io::{DmaFile, OpenOptions};
+use glommio::prelude::*;
+use glommio::sync::RwLock as GlommioRwLock;
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::path::PathBuf;
+use std::sync::{Arc, Mutex, RwLock};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use thiserror::Error;
+use bitflags::bitflags;
 
-/*
-** The type used to represent a page number.  The first page in a file
-** is called page 1.  0 is used to represent "not a page".
-** Using u64 for the love of game Sqlite3 use u32.
-*/
-
+/// The type used to represent a page number.  The first page in a file
+///  is called page 1.  0 is used to represent "not a page".
+///  Using u64 for the love of game Sqlite3 use u32.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 struct Pgno (u64);
- 
+
 #[derive(Error, Debug)]
 pub enum PgnoError {
     #[error("page number must be >= 1")]
@@ -27,7 +31,7 @@ impl Pgno {
         }
         Ok (Pgno(n))
     }
-    
+
     pub fn as_u64(self) -> u64 { self.0 }
 
     /// Convert to on-disk u32 representation (fail if too large).
@@ -48,6 +52,31 @@ impl Pgno {
     }
 }
 
+pub struct Page {
+    pub pgno: Pgno,
+    flags: AtomicUsize,
+    pin: AtomicUsize,
+    data: UnsaffeCell, //Data as bytes
+}
+
+unsafe impl Send for Page {}
+unsafe impl Sync for Page {}
+
+impl Page {
+}
+
+pub struct PageCache {
+    map: RwLock<HashMap<u64, Arc<Page>>>,
+    lru: Mutex<VecDeque<u64>>,
+    capacity: usize,
+    page_size: usize,
+}
+
+impl PageCache {
+
+}
+
+
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum PagerState { Closed, Open, Reader, Writer, WriterLocked }
 
@@ -58,4 +87,22 @@ pub struct Pager {
     pub filename: PathBuf,
     pub wal_filename: Option<PathButh>,
     pub read_only: bool,
+    state: PagerState,
+    cache: PageCache,
+    dirty_set: Mutex<HashSet2<u64>>,
+    dma_file: DmaFile,
+}
+
+impl Pager {
+    pub async fn new(
+        filename: PathBuf,
+        page_size: usize,
+        cache_capacity: usize,
+        read_only: bool
+    ) -> Result<Self, PageError> {
+        let dma_file = OpenOptions::new().read(true).write(!read_only).dma(true).open(&filename).await.map_
+
+
+
+    }
 }
